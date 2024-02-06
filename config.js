76777861738056a0d5a9c44be90025a13bb810bd
Clydiaprod <56416291+Clydiaprod@users.noreply.github.com>
2024-02-06 19:50:41 +0100

    pushhhhh

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   TOKEN: "",
-    ownerID: ["273508770988949505", ""],
+  ownerID: ["1004206704994566164", ""],
   botInvite: "",
   supportServer: "",
   mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
-  status: 'LISTENING MUSIC',
+  status: 'RTXX GG',
   commandsDir: './commands',
   language: "en",
   embedColor: "00fbff",
